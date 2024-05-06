e314ae353d43195b6e0195c0500df9c001d1d1ad
Michał Ziętera <m.zietera@binarapps.com>
2024-05-06 16:36:59 +0200

    Update model for coreML, detect objects using vision

--- CoreMLTesting/CoreMLViewController.swift
+++ CoreMLTesting/CoreMLViewController.swift
@@ -17,6 +17,7 @@ final class CoreMLViewController: UIViewController {
     private let captureSession = AVCaptureSession()
     
     private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+    private var vnRequest: VNRequest!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +47,7 @@ final class CoreMLViewController: UIViewController {
             return
         }
         captureSession.sessionPreset = .vga640x480
+        
         captureSession.addInput(videoDeviceInput)
         captureSession.commitConfiguration()
     }
@@ -56,6 +58,11 @@ final class CoreMLViewController: UIViewController {
         let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
         if captureSession.canAddOutput(videoDataOutput) {
             captureSession.addOutput(videoDataOutput)
+//            if let videoConnection = videoDataOutput.connection(with: .video) {
+//                if videoConnection.isVideoRotationAngleSupported(0) {
+//                    videoConnection.videoRotationAngle = 0
+//                }
+//            }
             videoDataOutput.alwaysDiscardsLateVideoFrames = true
             videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
             videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
@@ -70,26 +77,43 @@ final class CoreMLViewController: UIViewController {
         previewLayer.frame = captureViewLayer.bounds
         captureViewLayer.addSublayer(previewLayer)
         
+//        let detectionLayer =
     }
     
     private func setupVision() {
-//        yolov8n.mlpackage
-//        guard let modelUrl = Bundle.main.url(forResource: "yolov8n", withExtension: "mlpackage") else {
-//            present(UIAlertController(title: "Could not load model", message: nil, preferredStyle: .alert), animated: true)
-//            return
-//        }
         do {
             let yolov8nModel = try yolov8n()
             let visionModel = try VNCoreMLModel(for: yolov8nModel.model)
-            let objectRecognition = VNCoreMLRequest(model: visionModel) { request, error in
-                
-            }
+            vnRequest = VNCoreMLRequest(model: visionModel)
         } catch {
             present(UIAlertController(title: error.localizedDescription, message: nil, preferredStyle: .alert), animated: true)
+        }
+    }
+    
+    private func process(vnObservations: [VNRecognizedObjectObservation]) {
+        for object in vnObservations {
+            print("\(object.labels[0].identifier) \(object.confidence)")
         }
     }
 }
 
 extension CoreMLViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection
+    ) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        do {
+            try imageRequestHandler.perform([vnRequest])
+            if let recognizedObjects = vnRequest.results as? [VNRecognizedObjectObservation] {
+                process(vnObservations: recognizedObjects)
+            }
+        } catch {
+            print(error)
+        }
+    }
 }
